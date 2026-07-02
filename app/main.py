ac3bc7fc8fd76651813a034daadba6cf57dd001b
Vladyslav Palamarchuk <fayttter@gmail.com>
2026-07-02 22:19:41 +0300

    implementation of imports

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from people.customer import Customer
+from people.cinema_staff import Cleaner
+
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customer_instances = []
+    for c_dict in customers:
+        customer_obj = Customer(name=c_dict["name"], food=c_dict["food"])
+        customer_instances.append(customer_obj)
+
+        CinemaBar.sell_product(product=customer_obj.food, customer= customer_obj)
+        cinema_hall = CinemaHall(hall_number= hall_number)
+        cleaner_staff = Cleaner(name=cleaner)
+
+        cinema_hall.movie_session(
+            movi_name=movie,
+            customers=customer_instances,
+            cleaning_staff=cleaner_staff
+        )
+
+
