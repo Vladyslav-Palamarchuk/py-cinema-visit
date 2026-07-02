a4c3b68786bd977b1b9da3a08f370bed6b29e049
Vladyslav Palamarchuk <fayttter@gmail.com>
2026-07-02 23:05:12 +0300

    fixed the errors 5

--- app/main.py
+++ app/main.py
@@ -4,22 +4,15 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
-    customer_instances = []
-    for c_dict in customers:
-        customer_obj = Customer(name=c_dict["name"], food=c_dict["food"])
-        customer_instances.append(customer_obj)
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    cinema_customers = []
 
-        CinemaBar.sell_product(product=customer_obj.food,
-                               customer=customer_obj)
-        cinema_hall = CinemaHall(hall_number=hall_number)
-        cleaner_staff = Cleaner(name=cleaner)
+    for customer in customers:
+        customer = Customer(customer["name"], customer["food"])
+        CinemaBar.sell_product(customer, customer.food)
+        cinema_customers.append(customer)
 
-        cinema_hall.movie_session(
-            movi_name=movie,
-            customers=customer_instances,
-            cleaning_staff=cleaner_staff
-        )
+    cinema_hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner)
+    cinema_hall.movie_session(movie, cinema_customers, cleaner)
